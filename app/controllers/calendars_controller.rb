f20b298ed2e8efbafa1bdd8df1cf0f7cc4757df2
yuka7110 <yuka.o.nep@gmail.com>
2021-02-06 15:15:14 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,9 +35,12 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      
+      wday_num = @todays_date + x
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
       @week_days.push(days)
     end
 
